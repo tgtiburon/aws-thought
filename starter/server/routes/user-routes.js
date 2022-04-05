5cc7d563bfcb89883b409520cda544b6b0cdf1a6
tgtiburon <tg.tiburon@gmail.com>
2022-04-05 17:27:02 -0400

    finished users routes

--- starter/server/routes/user-routes.js
+++ starter/server/routes/user-routes.js
@@ -23,9 +23,71 @@ router.get('/users', (req, res) => {
     if (err) {
       res.status(500).json(err); // an error occurred
     } else {
-      res.json(data);
+      res.json(data.Items);
     }
   });
 });
+
+// Get a specific user's thoughts
+router.get('/users/:username', (req, res) => {
+  console.log(`Querying for thought(s) from ${req.params.username}.`);
+
+  const params = {
+    TableName: table,
+    // specifies search criteria like WHERE in SQL
+    // We could use < = << and between to find range of
+    // values
+    KeyConditionExpression: '#un = :user',
+    // Sets up aliases
+    // #un is username etc.
+    // can't use time, date, user, so make aliases
+    ExpressionAttributeNames: {
+      '#un': 'username',
+      '#ca': 'createdAt',
+      '#th': 'thought'
+    },
+    // AttributeValue can also have alias
+    // :user for req.params.username
+    ExpressionAttributeValues: {
+      ':user': req.params.username,
+    },
+    // which columns to return
+    ProjectionExpression: '#th, #ca',
+    // false so descending order
+    ScanIndexForward: false, 
+  }
+  dynamodb.query(params, (err, data) => {
+    if (err) {
+      console.error("Unable to query. Error:", JSON.stringify(err,null,2));
+    } else {
+      console.log("Query Succeeded.");
+      res.json(data.Items);
+    }
+  });
+});
+
+
+// Create a new user at /api/users
+router.post('/users', (req, res) => {
+  const params = {
+    TableName: table,
+    Item: {
+      username: req.body.username,
+      createdAt: Date.now(),
+      thought: req.body.thought,
+    },
+  };
+  // database call
+  dynamodb.put(params, (err, data) => {
+    if (err) {
+      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+      res.status(500).json(err); //Error occured
+    } else {
+      console.log("Added item:", JSON.stringify(data, null, 2));
+      res.json({"Added": JSON.stringify(data,null, 2)});
+    }
+  });
+
+})
 
 module.exports = router;
